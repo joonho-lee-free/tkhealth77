4c3cccc21dfd6fa807f0df8bfee6d9f92ee0d301
wefun74 <wefun74@gmail.com>
2025-04-23 22:07:43 +0900

    🚀 tkhealth77 대시보드 초기 푸시

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgo9r4epKHiq2CrXpayDtgHB4M_lgzC68",
+    authDomain: "tkhealth-30380.firebaseapp.com",
+    projectId: "tkhealth-30380",
+    storageBucket: "tkhealth-30380.appspot.com",
+    messagingSenderId: "692900691095",
+    appId: "1:692900691095:web:babfe553aef6aa4dddfdd2",
+    measurementId: "G-0MYSSGWH84"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  
---
+++ node_modules/react-native-health-connect/lib/module/types/metadata.types.js
@@ -0,0 +1,19 @@
+export let DeviceType;
+(function (DeviceType) {
+  DeviceType[DeviceType["TYPE_UNKNOWN"] = 0] = "TYPE_UNKNOWN";
+  DeviceType[DeviceType["TYPE_PHONE"] = 2] = "TYPE_PHONE";
+  DeviceType[DeviceType["TYPE_SCALE"] = 3] = "TYPE_SCALE";
+  DeviceType[DeviceType["TYPE_RING"] = 4] = "TYPE_RING";
+  DeviceType[DeviceType["TYPE_HEAD_MOUNTED"] = 5] = "TYPE_HEAD_MOUNTED";
+  DeviceType[DeviceType["TYPE_FITNESS_BAND"] = 6] = "TYPE_FITNESS_BAND";
+  DeviceType[DeviceType["TYPE_CHEST_STRAP"] = 7] = "TYPE_CHEST_STRAP";
+  DeviceType[DeviceType["TYPE_SMART_DISPLAY"] = 8] = "TYPE_SMART_DISPLAY";
+})(DeviceType || (DeviceType = {}));
+export let RecordingMethod;
+(function (RecordingMethod) {
+  RecordingMethod[RecordingMethod["RECORDING_METHOD_UNKNOWN"] = 0] = "RECORDING_METHOD_UNKNOWN";
+  RecordingMethod[RecordingMethod["RECORDING_METHOD_ACTIVELY_RECORDED"] = 1] = "RECORDING_METHOD_ACTIVELY_RECORDED";
+  RecordingMethod[RecordingMethod["RECORDING_METHOD_AUTOMATICALLY_RECORDED"] = 2] = "RECORDING_METHOD_AUTOMATICALLY_RECORDED";
+  RecordingMethod[RecordingMethod["RECORDING_METHOD_MANUAL_ENTRY"] = 3] = "RECORDING_METHOD_MANUAL_ENTRY";
+})(RecordingMethod || (RecordingMethod = {}));
+//# sourceMappingURL=metadata.types.js.map
